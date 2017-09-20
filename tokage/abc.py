9dd8ef32aa367dc025f5e372c863bcb986e5945c
SynderBlack <frogermanz@gmail.com>
2017-09-20 15:44:11 +0300

    add more docs

--- tokage/abc.py
+++ tokage/abc.py
@@ -1,8 +1,97 @@
-import re
-
+"""Base Classes for the API."""
 
 class Anime:
-    """Represents a MAL Anime"""
+    """Represents a MAL Anime
+
+    Attributes
+    ----------
+    id : int
+        The Anime's ID.
+
+    title : str
+        The Series title.
+
+    type : str
+       The Anime's type. Can be `ONA`/`OVA`/`TV`/`Movie`.
+
+    synonyms : list[str]
+       Alternative names for the Anime.
+
+    image : str
+        The cover image URL for the Anime.
+
+    japanese_title : str
+        Japanese title of the Anime.
+
+    status : str
+        Airing status of the Manga.
+
+    episodes : int
+        Episode count of the Manga.
+
+    air_start : str
+        Publication start date.
+
+    air_end : str
+        Publication end date.
+
+    synopsis : str
+        Description of the Anime.
+
+    producers : list[list]
+        WIP - List of the Anime's producers.
+
+    licensors : list[list]
+        WIP - List of the Anime's licensors.
+
+    studios : list[list]
+        WIP - List of the Anime's studios
+
+    premiered : str
+        Premier season.
+
+    broadcast : str
+        Broadcast times.
+
+    genres : list[str]
+        List of the Anime's genres.
+
+    link : str
+        Link to the Anime on MAL.
+
+    score : tuple(int)
+        Tuple of (score, voters).
+
+    duration : str
+        Duration of the Anime (may be per episode).
+
+    rank : int
+        Anime's rank on the MAL board.
+
+    popularity : int
+        Popularity rank of the Anime.
+
+    members : int
+        Amount of members which have the Anime in their list.
+
+    favorites : int
+        Amount of favorites given to the Anime.
+
+    source : str
+        Type of source material. Can be `Manga` `Novel` or `Original`.
+
+    related : dict
+        WIP - May include :attr:`adaptation`, :attr:`prequel` and :attr:`sequel`. Can also be None.
+
+    adaptation : list[dict]
+        WIP - Adaptations of the Anime. May be None.
+
+    sequel : list[dict]
+        WIP - Sequels to the Anime, if any. May be None.
+
+    prequel : list[dict]
+        WIP - Prequels to the Anime, if any. May be None.
+    """
     def __init__(self, anime_id, **kwargs):
         self.id = anime_id
         self.title = kwargs.pop('title', None)
@@ -12,7 +101,8 @@ class Anime:
         self.japanese_title = kwargs.pop('japanese', None)
         self.status = kwargs.pop('status', None)
         self.episodes = kwargs.pop('episodes', None)
-        self.air_time = kwargs.pop('aired', None)
+        self._air_time = kwargs.pop('aired', None)
+        self.air_start, self.air_end = self._air_time.split(" to ")
         self.premiered = kwargs.pop('premiered', None)
         self.broadcast = kwargs.pop('broadcast', None)
         self.synopsis = kwargs.pop('synopsis', None)
@@ -73,6 +163,9 @@ class Manga:
     publish_start : str
         Publication start date.
 
+    publish_end : str
+        Publication end date.
+
     synopsis : str
         Description of the Manga.
 
@@ -110,10 +203,10 @@ class Manga:
         WIP - Adaptations of the Manga. May be None.
 
     sequel : list[dict]
-        WIP - Sequels to the manga, if any. May be None.
+        WIP - Sequels to the Manga, if any. May be None.
 
     prequel : list[dict]
-        WIP - Prequels to the manga, if any. May be None.
+        WIP - Prequels to the Manga, if any. May be None.
     """
 
     def __init__(self, manga_id, **kwargs):
@@ -186,26 +279,60 @@ class Character:
     def __init__(self, char_id, **kwargs):
         self.id = char_id
         self.link = kwargs.pop('link-canonical', None)
+        self.name = kwargs.pop('name', None)
         self.image = kwargs.pop('image', None)
         self.favorites = kwargs.pop('member-favorites', None)
         self.animeography = kwargs.pop('animeography', None) # TODO: Handle
-        self.mangaography = kwargs.pop('mangaography', None) # TODO: Handle
-        self.name = kwargs.pop('name', None)
+        self.mangaography = kwargs.pop('mangaography', None) # TODO: Handle        
         self.japanese = kwargs.pop('name-japanese', None)
         self.about = kwargs.pop('about', None)
         self.voice_actors = kwargs.pop('voice-actors', None) # TODO: Handle
 
 class Person:
-    """Represents a MAL Person (Voice Actors, Staff, etc.)"""
+    """Represents a MAL Person (Voice Actors, Staff, etc.)
+
+    Attributes
+    ----------
+    id : int
+        The Person's ID.
+
+    name : str
+        The Person's name.
+
+    link : str
+        Link to the Person on MAL.
+
+    image : str
+        Image URL of the Person.
+
+    favorites : int
+        Amount of favorites the Person has.
+
+    anime : list[dict]
+        WIP - Staff positions in Anime.
+
+    manga : list[dict]
+        WIP - Published Manga.
+
+    more : str
+        Additional info about the Person.
+
+    website : str
+        Link to the Person's website
+
+    voice_acting : list[dict]
+        WIP - List of voice acting roles the Person has.
+
+    """
     def __init__(self, person_id, **kwargs):
         self.id = person_id
         self.link = kwargs.pop('link-canonical', None)
         self.name = kwargs.pop('name', None)
         self.image = kwargs.pop('image', None)
+        self.favorites = kwargs.pop('member-favorites', None)
+        self.anime = kwargs.pop('anime-staff-position', None) # TODO: Handle
+        self.manga = kwargs.pop('published-manga', None) # TODO: Handle
         self.birthday = kwargs.pop('birthday', None)
         self.more = kwargs.pop('more', None)
-        self.favorites = kwargs.pop('member-favorites', None)
         self.website = kwargs.pop('website', None)
-        self.voice_acting = kwargs.pop('voice-acting-role', None)
-        self.anime = kwargs.pop('anime-staff-position', None)
-        self.manga = kwargs.pop('published-manga', None)
+        self.voice_acting = kwargs.pop('voice-acting-role', None) # TODO: Handle
