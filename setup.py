84c5e5b4c0d0a35638530cbe9a288bba48adf7d1
SynderBlack <frogermanz@gmail.com>
2018-06-03 01:05:31 +0300

    no need for version.txt anymore

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ from setuptools import setup
 requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
-with open("version.txt") as f:
-    version = f.read()  # TODO: handle this better
 
 setup(
     name='Tokage',
